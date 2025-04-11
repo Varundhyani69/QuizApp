b31e3541f91c69b58dcd811f28925a4c7c3e2517
Varundhyani69 <varundhyani29@gmail.com>
2025-04-11 23:41:11 +0530

    Symbol above and below

--- QuizApp/src/Components/Container.jsx
+++ QuizApp/src/Components/Container.jsx
@@ -3,21 +3,22 @@ import { useState, useEffect } from "react";
 import StartPage from "./StartPage";
 import QuestionBox from "./QuestionBox";
 import ScorePage from "./scorePage.jsx";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 
-export default function Container({updateDifficulty,n,number, genAns, ans, q, updateQ }) {
+export default function Container({ updateDifficulty, n, number, genAns, ans, q, updateQ }) {
   const [subm, setSubm] = useState(false);
   const [scorePage, setScorePage] = useState(false);
   const [score, setScore] = useState(0);
   const [reaction, setReaction] = useState("");
   const [retry, setRetry] = useState(false);
   const [reactionClass, setReactionClass] = useState("");
-  
+
   useEffect(() => {
     if (scorePage) {
-      if (score <= (50*n/100)) {
+      if (score <= (50 * n / 100)) {
         setReaction(`Try Again! You just scored ${score}/${n}`);
         setReactionClass("poor");
-      } else if (score > (50*n/100) && score < (80*n/100)) {
+      } else if (score > (50 * n / 100) && score < (80 * n / 100)) {
         setReaction(`Good! You scored ${score}/${n}`);
         setReactionClass("good");
       } else {
@@ -48,22 +49,48 @@ export default function Container({updateDifficulty,n,number, genAns, ans, q, up
     setRetry(true);
   }
 
+  const config = {
+    loader: { load: ["input/tex", "output/chtml"] },
+  };
+
   return (
-    <>
-    <h1 className='head'>Quiz App</h1>
-    <div className="Container">
-      
-      {!subm ? (
-        <StartPage updateDifficulty={updateDifficulty}  n={n} number={number} q={q} sub={sub} updateQ={updateQ} />
-      ) : !scorePage ? (
-        <QuestionBox updateScore={updateScore} finish={finish} genAns={genAns} ans={ans} />
-      ) : !retry ? (
-        <ScorePage reactionClass={reactionClass} reaction={reaction} restart={restart} />
-      ) : (
-        <StartPage updateDifficulty={updateDifficulty} n={n} q={q} sub={sub} updateQ={updateQ} />
-      )}
-    </div>
-    </>
-    
+    <MathJaxContext config={config}>
+      <div className="ContainerWrapper">
+        <h1 className="head">Quiz App</h1>
+        <div className="Container">
+          {!subm ? (
+            <StartPage
+              updateDifficulty={updateDifficulty}
+              n={n}
+              number={number}
+              q={q}
+              sub={sub}
+              updateQ={updateQ}
+            />
+          ) : !scorePage ? (
+            <QuestionBox
+              updateScore={updateScore}
+              finish={finish}
+              genAns={genAns}
+              ans={ans}
+            />
+          ) : !retry ? (
+            <ScorePage
+              reactionClass={reactionClass}
+              reaction={reaction}
+              restart={restart}
+            />
+          ) : (
+            <StartPage
+              updateDifficulty={updateDifficulty}
+              n={n}
+              q={q}
+              sub={sub}
+              updateQ={updateQ}
+            />
+          )}
+        </div>
+      </div>
+    </MathJaxContext>
   );
 }
